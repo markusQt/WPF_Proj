02a4b5ce1ca1d3d85c700958e2b569e8ba9bf372
Markus Wildner <markus-72@gmx.de>
2023-10-25 21:08:25 +0200

    setting up simple nav construct

---
+++ ToolBoard/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows.Input;
+using ToolBoard.Enums;
+
+namespace ToolBoard.ViewModels
+{
+    public  class MainViewModel : ObservableObject
+    {
+        #region FullProps
+        private PageId _PageId;
+        public PageId PageId
+        {
+            get { return _PageId; }
+            set { SetProperty(ref _PageId, value);}
+        }
+        #endregion
+
+        #region Commands
+        public ICommand CMD_ChangePage => new RelayCommand<PageId>(ChangePage);  
+        #endregion
+
+        public MainViewModel()
+        {
+
+        }
+
+        #region Methods
+
+        void ChangePage( PageId newPage)
+        {
+            PageId = newPage;
+        }
+        #endregion
+    }
+}
